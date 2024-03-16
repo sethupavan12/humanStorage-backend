a57d13ba9752ab2665912c45f0e4add5425e6abf
sethu <sethupavan12@gmail.com>
2024-03-16 19:54:03 +0000

    messy code untested

---
+++ backend/ai.py
@@ -0,0 +1,119 @@
+"""
+This file handles the AI part of our document storing and retrieval blockchain platform.
+
+
+The user asks a question, then following happens
+1. User's question is sent to backend to analyze what category of documents the user is asking for.
+2. Frontend gets what to search for and retrieves all the documents of that category.
+3. The documents are then sent to the backend to be vectorized along with the user's question.
+4. AI will answer the question like RAG.
+5. The answer is then sent to the frontend to be displayed.
+
+
+Types of documents:
+1. Medical Records
+2. Passports
+3. Emergency Information
+4. Education Transcripts
+5. Legal Contracts 
+6. Misc - Birth Certificate, Intellectual Property Documentation
+"""
+import sqlite3
+import chromadb
+
+from langchain_community.document_loaders import UnstructuredFileLoader
+from langchain_community import Chroma
+# from langchain.vectorstores.chroma import Chroma
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from langchain_community.embeddings.sentence_transformer import (
+    SentenceTransformerEmbeddings,
+)
+
+PERSISTENT_DIR="./data"
+# embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+# Chroma(persist_directory=PERSISTENT_DIR, embedding_function=embedding_function)
+
+class AI:
+    """
+    This class handles the AI part.
+    """
+
+    def __init__(self,openai=True):
+        self.client = app.config["client"]
+        # self.client = chromadb.HttpClient(host='localhost', port='8080')
+
+        if self.openai:
+            self.llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.8)
+            self.embeddings = OpenAIEmbeddings()
+        else:
+            self.llm=None
+            self.embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+
+    def load_documents_from_unstrctured_data(self, list_of_paths: List[str]):
+        """
+        Loads the documents from the unstructured data.
+        """
+        loader = UnstructuredFileLoader(list_of_paths)
+        self.docs = loader.load_and_split()
+        return self.docs
+    
+    def check_if_collection_exists(self, vector_db,collection_name):
+        """
+        Checks if the given collection exists.
+        """
+        if vectordb._collection.count() > 0:
+            return True
+        return False
+    
+    def create_collection_and_put_it_in_db(self,list_of_paths, collection_name):
+        docs = load_documents_from_unstrctured_data(list_of_paths)
+
+        vectordb = Chroma.from_documents(docs, embedding=self.embeddings, persist_directory = PERSISTENT_DIR, collection_name=collection_name)
+        vectordb.persist()
+        return vectordb
+        print("Collection created and persisted")
+
+    def load_collection_from_db(self, collection_name):
+        vectordb = Chroma(persist_directory= PERSISTENT_DIR, embedding_function=self.embeddings, collection_name=collection_name)
+        print(f"Number of collections in {collection_name} is {vectordb._collection.count()}")
+        return vectordb
+    
+    def get_retriever_for_given_vectordb(self, vectordb):
+        return vectordb.as_retriever()
+
+    def get_answer(self, question, db):
+        """
+        Gets the answer to the given question.
+        """
+        
+        chain = RetrievalQA.from_chain_type(llm=self.llm,
+                                    chain_type="stuff",
+                                    retriever=db)
+        response = chain(question)
+        return response
+
+    
+
+
+
+class VectorStore:
+    """
+    Takes the list of strings and converts it into a vector.
+    """
+
+    def __init__(self):
+        self.client = chromadb.Client()
+
+
+    def vectorize(self, text):
+        """
+        Vectorizes the given text.
+        """
+        
+
+
+    
+
+
+
---
+++ backend/app.py
@@ -0,0 +1,76 @@
+from flask import Flask, render_template, request, jsonify, Response
+from flask_cors import CORS
+from ai import AI
+from langchain_community import Chroma
+
+from langchain_community.embeddings.sentence_transformer import (
+    SentenceTransformerEmbeddings,
+)
+from dotenv import load_dotenv
+load_dotenv()
+
+
+app = Flask(__name__)
+CORS(app, origins="*")
+
+# add client = chromadb.Client() to app.config so that it can be accessed by the routes
+client_object = chromadb.PersistentClient()
+app.config["client"] = client_object
+
+
+ai = AI()
+
+PERSISTENT_DIR="./data"
+embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+vector_db = Chroma(persist_directory=PERSISTENT_DIR, embedding_function=embeddings)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/ask", methods=["POST"])
+def ai():
+    """
+    Populates the collection with the given file path.
+
+    Once the documents are fetched from IFPS/Filecoin,
+    they are sent to this route to be vectorized and stored in the collection.
+    """
+    data = request.json
+    list_of_paths = data["paths"]# list of strings
+    collection_name = data["collection_name"]
+
+    # DB STUFF
+    # check if the collection exists
+    collection_exists = ai.check_if_collection_exists(vector_db, collection_name)
+    if not collection_exists: 
+        # create the collection and put it in the database
+        vector_db = ai.create_collection_and_put_it_in_db(list_of_paths, collection_name)
+    else:
+        # load the collection from the database
+        vector_db = ai.load_collection_from_db(collection_name)
+
+    # get the retriever for the collection
+    retriever = ai.get_retriever_for_given_vectordb(vector_db)
+
+    answer = ai.get_answer(data["question"], retriever)
+    
+    return jsonify({"answer": answer})
+
+
+@app.route("/add_live", methods=["POST"])
+def live():
+    data = request.json
+    audio_text = data["text"]
+    first_time = data["first_time"]
+    if first_time:
+        video_path = agent.get_video_path(audio_text, first_time=True)
+    else:
+        video_path = agent.get_video_path(audio_text)
+    return jsonify({"video_path": video_path})
+
+if __name__ == '__main__':
+    # app.run(port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
+
+    
